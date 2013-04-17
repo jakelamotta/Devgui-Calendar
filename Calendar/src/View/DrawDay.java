30ea300ca47ad2045d2d0a436fb8c7be9ba9af12
Genius <Genius@130.243.202.225>
2013-04-17 13:32:45 +0200

    added the graphic

--- Calendar/src/View/DrawDay.java
+++ Calendar/src/View/DrawDay.java
@@ -5,8 +5,10 @@
 package View;
 
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -28,7 +30,14 @@ public class DrawDay implements Drawable {
     
     //@Override
     public void drawString(Graphics g) {
-    	
+    
+    	Graphics2D g2 = (Graphics2D) g.create();
+        
+    	//newlines
+        g2.setColor(Color.darkGray);
+        g2.fillRoundRect(d * 30 + 46, week * 29 + 81, 25, 25,10,10);
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.75f));
+        
         if(day.format(calculatedate.gettime()).equals(day.format(date))){
          g.setColor(Color.red);
           g.drawString(day.format(calculatedate.gettime()), d * 30 + 46 + 4,
