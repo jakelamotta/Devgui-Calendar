7ed050c4013f4304a7f5dcd88beb176c6eecfcdf
bigbigguoguo <bigbigguoguo@nl119-200-189.student.uu.se>
2013-05-23 17:36:59 +0200

    add mouseListener for move

---
+++ src/controller/DayMouseListener.java
@@ -0,0 +1,142 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import javax.swing.JPanel;
+
+import view.Calendar;
+import view.DrawDayforselected;
+import view.Drawable;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import view.EventPanel;
+/**
+ *
+ * @author bigbigguoguo
+ */
+public class DayMouseListener extends MouseAdapter {
+    private SimpleDateFormat present= new SimpleDateFormat("yyyyMMd")	;
+    private int d;
+    private int week;
+    private Date today=new Date();
+    int row;
+    String taskdate;
+    //float leftX=(float)((d-2) * 45 + 41);  
+    //float upY=(float)((week-2) * 45);  
+    //float rightX=(float)((d) * 45 + 41);  
+    //float downY=(float)((week) * 45);  
+    public void getxpoint(float x,float y){
+row= EventPanel.table.getRowCount();
+
+if((108.5-22.5<x)&&(x<108.5+22.5)){
+                 d=1;
+                 getypoint(y);}
+if((153.5-22.5<x)&&(x<153.5+22.5)){
+                 d=2;
+                 getypoint(y);}
+if((198.5-22.5<x)&&(x<198.5+22.5)){
+                 d=3;
+                 getypoint(y);}
+if((243.5-22.5<x)&&(x<243.5+22.5)){
+                 d=4;
+                 getypoint(y);}
+if((288.5-22.5<x)&&(x<288.5+22.5)){
+                 d=5;
+                 getypoint(y);}
+if((333.5-22.5<x)&&(x<333.5+22.5)){
+                 d=6; 
+                 getypoint(y);}
+if((378.5-22.5<x)&&(x<378.5+22.5)){
+                 d=7;
+                 getypoint(y);}
+if((336<x)&&(x<396)&&(15<y)&&(y<45))
+{System.out.print(row);
+    for(int i=0;i<row;i++){
+   
+        taskdate=(String) EventPanel.table.getValueAt(i,1);
+ System.out.print((String) EventPanel.table.getValueAt(i,1));
+        if(taskdate.equals(present.format(today))){
+//model.TableModel.removeRowforDisplay(i);
+    System.out.print((String) EventPanel.table.getValueAt(i,0));
+    
+}      
+}
+}
+}
+     public void getypoint(float y){
+if((98.5-22.5<y)&&(y<98.5+22.5)){
+                 week=1;
+                 Calendar.Selected.clear();
+                 Calendar.Selected2.clear();
+ addDrawableForselected(new DrawDayforselected(d,week));
+}
+if((143.5-22.5<y)&&(y<143.5+22.5)){
+                 week=2;
+                 Calendar.Selected.clear();
+                 Calendar.Selected2.clear();
+ addDrawableForselected(new DrawDayforselected(d,week));
+}
+if((188.5-22.5<y)&&(y<188.5+22.5)){
+                 week=3;
+                 Calendar.Selected.clear();
+                 Calendar.Selected2.clear();
+ addDrawableForselected(new DrawDayforselected(d,week));
+}
+if((233.5-22.5<y)&&(y<233.5+22.5)){
+                 week=4;
+                 Calendar.Selected.clear();
+                 Calendar.Selected2.clear();
+ addDrawableForselected(new DrawDayforselected(d,week));
+}
+if((278.5-22.5<y)&&(y<278.5+22.5)){
+                 week=5;
+                 Calendar.Selected.clear();
+                 Calendar.Selected2.clear();
+ addDrawableForselected(new DrawDayforselected(d,week));
+}
+if((323.5-22.5<y)&&(y<323.5+22.5)){
+                 week=6;
+                 Calendar.Selected.clear();
+                 Calendar.Selected2.clear();
+ addDrawableForselected(new DrawDayforselected(d,week));
+}
+
+    }
+      public void addDrawableForselected(Drawable d){
+	    Calendar.Selected.add(d);
+      }    
+    public void mouseClicked(MouseEvent e) {
+
+    
+      	     if (e.getButton() == MouseEvent.BUTTON1) {
+      	     if (e.getClickCount() == 1) {
+             float x=e.getX();
+             float y=e.getY();
+             getxpoint( x, y);
+                  //float distance=(x-centerX)*(x-centerX)+(y-centerY)*(y-centerY);
+             //System.out.println("work");
+                 
+                
+                 
+                 //(x-centerX)*(x-centerX)+(y-centerY)*(y-centerY);
+               //P.setLocation(e.getX(),e.getY());(x-centerX)*(x-centerX)+(y-centerY)*(y-centerY);
+            //week * 29 + 81 + 20
+             
+      	    	
+                     //Calendar.Calendars.add(0,new(DrawMonth(Color.getColor(gray))));
+                 }
+            }
+}
+}
+
---
+++ src/model/TableModel.java
@@ -0,0 +1,185 @@
+package model;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+import controller.XMLHandler;
+
+/**
+ * This class defines the table model 
+ * @author Deha
+ *
+ */
+@Root
+public class TableModel extends AbstractTableModel {
+	
+	private static final long serialVersionUID = 1585636867604774966L;
+	private  XMLHandler xmlh;
+    private String[] columnNames = {"Event",
+    								"DueDate",
+    								"Category",
+    								"Priority",
+    								"Edit",
+    								"Delete"};
+    @ElementList
+    private static ArrayList<EventTable> data;
+    
+	
+	/**
+	*  Initiate XML handler and load data from XML 
+	*  into an ArrayList. If the file is empty or do  
+	*  not exist it initiates the ArrayList. 
+	*/
+    public TableModel() {
+    	xmlh = new XMLHandler();
+		xmlh.readXML(this, XMLHandler.XML_ITEMS);
+		if (data == null) {
+			data = new ArrayList<EventTable>();
+		}
+    }
+    
+    /**
+	 * Decides which columns are possible to edit
+	 * @param row the row being queried
+	 * @param col the column being queried
+	 * @return true if the specified column is editable, otherwise false.
+	 */	 
+	 public boolean isCellEditable(int row, int col) {
+		 if (col >= 4) {
+              return true;
+          } else {
+              return false;
+          }
+	    }
+
+	/**
+	* Adds row into the table and updates XML
+	* @param values contain values that should be added into the row
+	*/	
+    public void addRow(Object[] values){
+    	EventTable t = new EventTable();
+    	t.setEventName((String) values[0]);
+    	t.setEventDueDate((String) values[1]);
+    	t.setEventCategory((String) values[2]);
+    	t.setEventPriority((Integer) values[3]);
+    	t.setbutton1((String) values[4]);
+    	t.setbutton2((String) values[5]);
+    	data.add(t);
+    	xmlh.writeXML(this, XMLHandler.XML_ITEMS);
+        fireTableDataChanged();
+	}
+    
+    /**
+     * Adds row into the table according to row number and updates XML	
+     * @param row defines where the new row will be added
+     * @param values contain values that should be added into the row
+     */
+    public void addRow(int row, Object[] values){
+    	EventTable t = new EventTable();
+    	t.setEventName((String) values[0]);
+    	t.setEventDueDate((String) values[1]);
+    	t.setEventCategory((String) values[2]);
+    	t.setEventPriority((Integer) values[3]);
+    	t.setbutton1((String) values[4]);
+    	t.setbutton2((String) values[5]);
+    	data.add(row, t);
+    	xmlh.writeXML(this, XMLHandler.XML_ITEMS);
+        fireTableDataChanged();
+	}
+
+	/**
+	*  Remove row from the table and updates XML
+	*  @param row the row being queried
+	*/			 
+	 public void removeRow(int row){
+	    data.remove(row);
+	    xmlh.writeXML(this, XMLHandler.XML_ITEMS);
+	    fireTableDataChanged();
+   }
+ public static void removeRowforDisplay(int row)
+ {
+ data.remove(row);
+ 
+ }
+	/**
+	* Returns number of columns.  
+	* @return columnnNames.length number of columns
+	*/
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	/**
+	* Returns number of rows  
+	* @return data.size number of rows
+	*/
+ 	public int getRowCount() {
+		
+		 return data.size();
+	}
+
+	/**
+	* Returns specific column name	
+	* @param col the column being queried
+	* @return columnNames[col] name of the column
+	*/
+ 	 public String getColumnName(int col){
+		 return columnNames[col];
+	 }
+
+	 /**
+	 * Return value for specific cell in the table
+	 * @param row the row being queried
+	 * @param col the column being queried
+	 * @return The value of the cell at row row and column col in the table.
+	 */
+	 public Object getValueAt(int row, int col)
+	    {
+		 switch (col) {
+		    case 0: return data.get(row).getEventName();
+		    case 1: return data.get(row).getEventDueDate();
+		    case 2: return data.get(row).getEventCategory();
+		    case 3: return data.get(row).getEventPriority();
+		    case 4: return data.get(row).getbutton1();
+		    case 5: return data.get(row).getbutton2();
+		    default: return null;
+		  }
+	    }
+	 
+	 
+	 /**
+		 * Sets value for specific cell in the table. Updates the xml file
+		 * and if DEBUG param is set to true prints out log of the changes made.
+		 * DEBUG param is default set to true.
+		 * @param value new alue for the cell
+		 * @param row the row being queried
+		 * @param col the column being queried
+		 */		
+		public void setValueAt(Object value, int row, int col){
+		
+			 switch (col) {
+			    case 0:  data.get(row).setEventName((String) value);
+			    		break;
+			    case 1:  data.get(row).setEventDueDate((String) value);
+			    		break;
+			    case 2:  data.get(row).setEventCategory((String) value);
+			    		break;
+			    case 3:  data.get(row).setEventPriority((Integer) value);
+			    		break;
+			    case 4:  data.get(row).setbutton1((String) value);
+			    		break;
+			    case 5:  data.get(row).setbutton2((String) value);
+			    		break;
+			    default: System.err.println("Error setting new value");
+			    
+			  }
+			
+	    	 xmlh.writeXML(this, XMLHandler.XML_ITEMS);
+			 fireTableCellUpdated(row,col);
+
+		 }
+}
